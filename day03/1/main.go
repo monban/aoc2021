da55e1130099fdce133521237ee21cfcf09c81f1
Ian Flynn <himitsu.monban@gmail.com>
2022-08-01 01:10:11 -0400

    refactor

--- day03/1/main.go
+++ day03/1/main.go
@@ -4,32 +4,60 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
 func main() {
-	bitcounts := [12]uint{}
+	var bitcounts []uint // Count of each bit
+	var bitLength int    // Total width of incoming bitstream
+	var bitMask uint     // Mask representing the bitstream width
+	var records uint     // Number of records processed
+	var gamma uint       // Output variable
+	var epsilon uint     // Output variable
+
 	reader := bufio.NewReader(os.Stdin)
 	for {
 		line, err := reader.ReadString('\n')
 		if err != nil {
+			fmt.Println(err.Error())
 			break
 		}
 		line = strings.Trim(line, "\n")
-		for i, k := range line {
-			if k == '1' {
-				bitcounts[i] += 1
+		record, err := strconv.ParseUint(line, 2, 16)
+		if err != nil {
+			continue
+		}
+		records++
+
+		if bitLength == 0 {
+			fmt.Println("Setup...")
+			if err != nil {
+				fmt.Printf("Error reading first line: %s\n", err.Error())
+				return
+			}
+			bitLength = len(line)
+			bitcounts = make([]uint, len(line), len(line))
+			for i := 0; i < bitLength; i++ {
+				bitMask = bitMask | (1 << i)
+			}
+			fmt.Printf("bitLength: %d\nbitMask: %032b\nbitcounts: %+v\n", bitLength, bitMask, bitcounts)
+			fmt.Println("Processing...")
+		}
+		fmt.Printf("Record %04d: %016b\n", records-1, record)
+		for i := 0; i < bitLength; i++ {
+			if record&(1<<i) != 0 {
+				bitcounts[bitLength-i-1] += 1
 			}
 		}
 	}
-	fmt.Printf("%+v\n", bitcounts)
-	var gamma uint
+	fmt.Printf("bitcounts: %+v\n", bitcounts)
 	for _, k := range bitcounts {
 		gamma = gamma << 1
-		if k > 500 {
+		if k >= (records >> 1) {
 			gamma += 1
 		}
 	}
-	epsilon := (^gamma) & 0xFFF
-	fmt.Printf("%8s : %012b\n%8s : %012b\n%8s : %d\n", "gamma", gamma, "epsilon", epsilon, "answer", gamma*epsilon)
+	epsilon = (^gamma) & bitMask
+	fmt.Printf("%8s : %d\n%8s : %016b\n%8s : %016b\n%8s : %d\n", "records", records, "gamma", gamma, "epsilon", epsilon, "answer1", gamma*epsilon)
 }
